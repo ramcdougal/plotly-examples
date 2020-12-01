327ca25f801c6327d922d5c19cbc85b4e10e3350
Robert A McDougal <robert.mcdougal@yale.edu>
2020-11-30 23:45:35 -0500

    pie chart via flask

---
+++ flask-pie-chart.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template_string
+
+my_template = """
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<div id="myGraph"></div>
+<script>
+    Plotly.newPlot("myGraph", {{data|tojson}}, {"title": "{{state}}"})
+</script>
+"""
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template_string(
+        my_template,
+        state="Connecticut",
+        data=[
+            {
+                "values": [12.7, 87.3],
+                "labels": ["smokers", "non-smokers"],
+                "type": "pie",
+            }
+        ],
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
