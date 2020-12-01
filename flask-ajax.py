efe47adcaf4162b611f7c598568595078e57a5ad
Robert A McDougal <robert.mcdougal@yale.edu>
2020-12-01 13:22:35 -0500

    flask ajax graph example

---
+++ flask-ajax.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template_string, jsonify
+
+my_template = """
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+<div id="myGraph"></div>
+<script>
+(
+    fetch("/api/data")
+    .then(response => response.json())
+    .then(function (data) {Plotly.newPlot("myGraph", data)})
+)
+</script>
+"""
+
+app = Flask(__name__)
+
+
+@app.route("/api/data")
+def data():
+    return jsonify([{"x": [1, 2, 3, 4, 5], "y": [1, 4, 9, 16, 25], "type": "scatter"}])
+
+
+@app.route("/")
+def index():
+    return render_template_string(my_template)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
